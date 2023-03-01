0135b9a16f81b40d844934d0b72e16acd54f5ef1
handmadeCode <sbae5617@gmail.com>
2023-03-01 15:08:33 +0900

    헤더 타이틀 스타일 변경

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -2,7 +2,7 @@
     <v-toolbar>
         <v-container class="fill-height d-flex align-center">
             <v-img src="./../assets/logo.png" max-width="36" class="mr-2" />
-            <v-toolbar-title text="Vue Todo" />
+            <v-toolbar-title text="Vue Todo" class="font-weight-bold text-h5" />
         </v-container>
     </v-toolbar>
 </template>
